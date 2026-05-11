c4c40e7ffe14571f774a5816ffb55cb1cbb2d0ff
sheetalrkamble01 <sheetalrkamble01@gmail.com>
2026-05-11 19:04:10 +0000

    fix test for flask app

--- test_app.py
+++ test_app.py
@@ -1,4 +1,6 @@
-from app import add
+from app import app
 
-def test_add():
-    assert add(2, 3) == 5
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.data == b"CI/CD Pipeline Working!"
